cfd91018d5ff9e311acac2b0dc282504eac28f6a
WUMBAch-WEB <70854414+WUMBAch-WEB@users.noreply.github.com>
2021-07-13 01:33:05 +0300

    Delete GrandHomeWork directory

--- GrandHomeWork/src/main/java/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-import models.Course;
-
-import java.util.Optional;
-
-public interface CourseRepository {
-    public Optional<Course> findById(Long id);
-    public Optional<Course> findByName(String name);
-}
