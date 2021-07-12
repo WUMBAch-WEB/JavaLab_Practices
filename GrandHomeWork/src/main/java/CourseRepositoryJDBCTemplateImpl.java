5f8c64c02fb3491fef890506bfe4ed59240257de
WUMBAch-WEB <70854414+WUMBAch-WEB@users.noreply.github.com>
2021-07-12 21:38:40 +0300

    Update - 75% done

--- GrandHomeWork/src/main/java/CourseRepositoryJDBCTemplateImpl.java
+++ GrandHomeWork/src/main/java/CourseRepositoryJDBCTemplateImpl.java
@@ -1,14 +1,9 @@
-import models.Course;
-import models.Student;
-import models.Teacher;
-import org.springframework.dao.EmptyResultDataAccessException;
+import models.*;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class CourseRepositoryJDBCTemplateImpl implements CourseRepository{
@@ -22,64 +17,114 @@ public class CourseRepositoryJDBCTemplateImpl implements CourseRepository{
     }
 
     //language=SQL
-    private static final String SQL_FIND_BY_ID = "select *, " +
-            " from course as c" +
-            "left join courses_students_relation csr on c.id = csr.course_id " +
-            "left join courses_teachers_relation as сtr on ctr.teacher_id = c.teacher_id where c.id = ? order by c.id ";
+    private static final String FIND_BY_ID = "select * from course where id = ?";
 
-    private final RowMapper<Teacher> teacherRowMapper = (row, rowNumber) -> {
-        Integer id = row.getInt("teacher_id");
-        String firstName = row.getString("teacher_first_name");
-        String lastname = row.getString("teacher_last_name");
-        String exp = row.getString("experience");
+    //language=SQL
+    private static final String FIND_TEACHERS_BY_COURSE_ID =
+            "select * from teacher right join courses_teachers_relation ctr on teacher.teacher_id = ctr.teacher_id" +
+                    " where course_id = ?";
 
-        return new Teacher(id, firstName, lastname, exp);
+    //language=SQL
+    private static final String FIND_STUDENTS_BY_COURSE_ID =
+            "select * from student right join courses_students_relation csr on student.student_id = csr.student_id" +
+                    " where course_id = ?";
+
+    //language=SQL
+    private static final String FIND_LESSONS_BY_COURSE_ID =
+            "select * from lesson right join courses_lessons_relation clr on lesson.lesson_id = clr.lesson_id" +
+                    " where course_id = ?";
+
+    //language=SQL
+    private static final String FIND_COURSE_BY_NAME =
+            "select * from course where name = ?";
+
+
+
+
+    private RowMapper<Teacher> teacherRowMapper = (rs, rowNum) -> {
+        Long id = rs.getLong("teacher_id");
+        String name = rs.getString("first_name");
+        String surname = rs.getString("last_name");
+        String experience = rs.getString("experience");
+        return new Teacher(id, name, surname, experience);
     };
 
-    private final RowMapper<Course> courseRowMapper = (row, rowNumber) -> {
-        Integer id = row.getInt("id");
-        String name = row.getString("name");
-        String dateStart = row.getString("start_date");
-        String dateEnd = row.getString("end_date");
-        Teacher teacher = teacherRowMapper.mapRow(row, rowNumber);
 
-        Course course = new Course(id, name, dateStart, dateEnd, teacher);
-        course.setStudentList(new ArrayList<>());
-        return course;
+    private final RowMapper<Student> studentRowMapper = (rs, rowNum) -> {
+        return new Student(rs.getLong("student_id"),
+                rs.getString("first_name"),
+                rs.getString("last_name"),
+                rs.getString("group_number"));
+
     };
 
-    private final RowMapper<Student> studentRowMapper = (row, rowNumber) -> {
-        Integer id = row.getInt("student_id");
-        String firstName = row.getString("student_first_name");
-        String lastName = row.getString("student_last_name");
-        String groupNumber = row.getString("group_number");
 
-        return new Student(id, firstName, lastName, groupNumber);
+    private RowMapper<Lesson> lessonRowMapper = (rs, rowNum) -> {
+        return new Lesson(
+                rs.getLong("lesson_id"),
+                rs.getString("name"),
+                rs.getString("day_week"),
+                rs.getString("time"));
     };
 
-    private final ResultSetExtractor<Course> courseExtractor2 = resultSet -> {
-        Course course = null;
-        if (resultSet.next()) {
-            course = courseRowMapper.mapRow(resultSet, resultSet.getRow());
-            course.setStudentList(new ArrayList<>());
 
-            do {
-                Integer id = resultSet.getObject("student_id", Integer.class);
-                if (id != null) {
-                    Student student = studentRowMapper.mapRow(resultSet, resultSet.getRow());
-                    course.getStudentList().add(student);
-                }
-            } while (resultSet.next());
-        }
-        return course;
+
+    private ResultSetExtractor<Course> courseResultSetExtractor = rs -> {
+        rs.next();
+        return new Course(rs.getLong("id"),
+                rs.getString("name"),
+                rs.getString("date_start"),
+                rs.getString("date_end"));
     };
 
     @Override
-    public Optional<Course> findById(Integer id) {
+    public Optional<Course> findById(Long id) {
+        Course course;
         try {
-            return Optional.of(jdbcTemplate.query(SQL_FIND_BY_ID, courseExtractor2, id));
-        } catch (EmptyResultDataAccessException e) {
+            course = jdbcTemplate.query(FIND_BY_ID, courseResultSetExtractor, id);
+        } catch (Exception e) {
             return Optional.empty();
         }
+        course.setTeacherList(jdbcTemplate.query(FIND_TEACHERS_BY_COURSE_ID,
+                teacherRowMapper,
+                id));
+        course.setStudentList(jdbcTemplate.query(
+                FIND_STUDENTS_BY_COURSE_ID,
+                studentRowMapper,
+                id));
+
+        course.setLessonList(jdbcTemplate.query(
+                FIND_LESSONS_BY_COURSE_ID,
+                lessonRowMapper,
+                id));
+
+        return Optional.of(course);
     }
+
+    @Override
+    public Optional<Course> findByName(String name) {
+        Course course;
+        try {
+            course = jdbcTemplate.query(FIND_COURSE_BY_NAME, courseResultSetExtractor, name);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+        var list = jdbcTemplate.query(FIND_TEACHERS_BY_COURSE_ID,
+                teacherRowMapper,
+                course.getId());
+        course.setTeacherList(list);
+        course.setStudentList(jdbcTemplate.query(
+                FIND_STUDENTS_BY_COURSE_ID,
+                studentRowMapper,
+                course.getId()));
+
+        course.setLessonList(jdbcTemplate.query(
+                FIND_LESSONS_BY_COURSE_ID,
+                lessonRowMapper,
+                course.getId()));
+
+        return Optional.of(course);
+    }
+
+
 }
